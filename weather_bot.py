5805679d0ae70c4536b47e597ba79dd12ff024b5
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-12-07 03:17:21 +0400

    SET UP READY

--- weather_bot.py
+++ weather_bot.py
@@ -1,6 +1,21 @@
 
+# Libraries
 import os
+import datetime as dt
+import requests
 
-api_key = os.environ.get("WEATHERAPI")
+# CONSTANTS
 
-print(type(api_key))
+# API KEY STORED IN ENVIRONMENT VARIABLE --> WEATHERAPI
+API_KEY = os.environ.get("WEATHERAPI")
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+CITY = "DUBAI"
+
+
+url = BASE_URL + "appid="+  API_KEY + "&q=" + CITY
+
+response = requests.get(url).json()
+
+print(dict(response).keys())
+
+
