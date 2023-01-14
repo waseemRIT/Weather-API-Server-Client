c8d013a3878ef2740f584da734673ab1a6f34e47
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2023-01-14 21:37:30 +0400

    Menu of Cities Added

--- weather_bot.py
+++ weather_bot.py
@@ -9,24 +9,42 @@ from tabulate import tabulate
 # API KEY STORED IN ENVIRONMENT VARIABLE --> WEATHERAPI
 API_KEY = os.environ.get("WEATHERAPI")
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
-CITY = "DUBAI"
+CITIES = ["DUBAI", "NEW YORK", "LONDON", "TOKYO", "BEIJING"]
 
-# Creating the API request URL
-url = BASE_URL + "appid="+ API_KEY + "&q=" + CITY
 
-# Making the API request and storing the response
-response = requests.get(url).json()
+def get_weather_data(city):
+    # Creating the API request URL
+    url = BASE_URL + "appid=" + API_KEY + "&q=" + city
 
-# Extracting the relevant data from the response
-weather_data = {
-    'Temperature': response['main']['temp'],
-    'Pressure': response['main']['pressure'],
-    'Humidity': response['main']['humidity'],
-    'Min Temperature': response['main']['temp_min'],
-    'Max Temperature': response['main']['temp_max'],
-    'Wind Speed': response['wind']['speed'],
-    'Weather Description': response['weather'][0]['description'],
-}
+    # Making the API request and storing the response
+    response = requests.get(url).json()
 
-# Printing the data in a well formatted table
-print(tabulate(weather_data.items(), headers=['Weather Parameter', 'Value'], tablefmt='fancy_grid'))
+    # Extracting the relevant data from the response
+    weather_data = {
+        'Temperature': response['main']['temp'],
+        'Pressure': response['main']['pressure'],
+        'Humidity': response['main']['humidity'],
+        'Min Temperature': response['main']['temp_min'],
+        'Max Temperature': response['main']['temp_max'],
+        'Wind Speed': response['wind']['speed'],
+        'Weather Description': response['weather'][0]['description'],
+    }
+    return weather_data
+
+
+def print_weather_data(city):
+    weather_data = get_weather_data(city)
+    print(tabulate(weather_data.items(), headers=['Weather Parameter', 'Value'], tablefmt='fancy_grid'))
+
+
+def show_city_menu():
+    print("Select a city:")
+    for i, city in enumerate(CITIES):
+        print(f"{i + 1}. {city}")
+    city_index = int(input()) - 1
+    city = CITIES[city_index]
+    print_weather_data(city)
+
+
+if __name__ == '__main__':
+    show_city_menu()
